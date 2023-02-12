128196801424e25c4d4964afb1c91c613299c0f2
YVakhnyak <namejfk@gmail.com>
2023-02-12 04:05:13 +0200

    1 and 2 tasks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,20 @@
+import user from '../json/user.json';
+import data from '../json/data.json';
+
+import '../index.css';
+import { Profile } from '../components/Profile/Profile.jsx';
+import { Statistics } from '../components/Statistics/Statistics.jsx';
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className="thumb">
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Uploads stats" stats={data} />
     </div>
   );
 };
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,34 @@
+import PropTypes from 'prop-types';
+
+// export function getRandom(min, max) {
+//   return Math.ceil(Math.random() * (max - min) + min);
+// }
+
+export const Statistics = ({ title, stats }) => {
+  return (
+    <section className="statistics">
+      <div className="statBox">
+        {(title.length > 0 || title == null) && (
+          <h2 className="title">{title}</h2>
+        )}
+
+        <ul className="statList">
+          {stats.map(data => (
+            <li className="item" key={data.id}>
+              <span className="label">{data.label}</span>
+              <span className="percentage">{data.percentage}%</span>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </section>
+  );
+};
+
+Statistics.propTypes = {
+  data: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    vielabelws: PropTypes.string.isRequired,
+    percentage: PropTypes.number.isRequired,
+  }),
+};
